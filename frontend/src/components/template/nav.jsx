91589e6dd6c8ccd19636cfc5bc2cfbd74a90a14c
Nathan <nathan.front.dev@gmail.com>
2022-06-10 16:27:07 -0300

    Segundo

--- frontend/src/components/template/nav.jsx
+++ frontend/src/components/template/nav.jsx
@@ -6,21 +6,29 @@ export default props =>
     <nav className='menu'>
       <a href="#/ ">
 <i className='fa fa-home'></i>
+
 Início
+<hr/>
       </a>
+      
       <a href='#/computadores'>
           <i className="fa fa-keyboard-o"></i>
+          
           Computadores
+          <hr/>
       </a>
       <a href='#/impressoras'>
         
           <i className="fa fa-print"></i>
+         
           Impressoras
-          
+          <hr/>
       </a>
       <a href='#/monitores'>
           <i className="fa fa-desktop"></i>
+          
           Monitores
+          <hr/>
       </a>
     </nav>
 </aside>
